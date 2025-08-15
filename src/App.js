ad51b36f3b650e466dc3f1b9d92435f4a2f950be
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-08-15 12:59:17 +0530

    Edit page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Homepage from "./Components/HomePage"
 import SignIn from "./Components/SignIn"; 
 import SignupPage from './Components/SingUp';
+import GetStartedPage from './Components/GetStartedpage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Homepage />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path='/signuppage' element={<SignupPage/>}/>
+      <Route path='/getstarted' element={<GetStartedPage/>}/>
     </Routes>
   );
 }

fab3bbaee18303a7fbcc6eb32ac36e134504d23a
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-08-29 20:51:37 +0530

    Backend integeration

--- Back-end/server.js
+++ Back-end/server.js
@@ -8,7 +8,6 @@ app.use(bodyParser.json());
 
 // Routes
 app.use("/api", questionRoutes);
-
 const PORT = 5021;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
